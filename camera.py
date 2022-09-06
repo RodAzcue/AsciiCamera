39b2036be2325123086941799c079410ef06a689
RodAzcue <rodazcue@gmail.com>
2022-09-05 21:50:44 -0500

    added parse args

--- camera.py
+++ camera.py
@@ -1,84 +1,72 @@
 import cv2
 import time
 from terminalTrueColors import *
-from sys import stdout
+from sys import stdout, argv
+import argparse
 
 
-cap = cv2.VideoCapture(0)
-cap.set(5, 60)
+def main(asciiChars, scale, color):
+    cap = cv2.VideoCapture(0)
+    cap.set(5, 60)
 
-framecount = 0
-prevMillis = 0
+    #print(cap.get(5)) # fps
+    width = int(cap.get(3))
+    height = int(cap.get(4))
+    #print("width: ", width)
+    #print("height: ", height)
 
-print(cap.get(5)) # fps
-width = int(cap.get(3))
-height = int(cap.get(4))
-
-print("width: ", width)
-print("height: ", height)
-"""
-def fpsCount():
-    global prevMillis
-    global framecount
-    millis = int(round(time.time() * 1000))
-    framecount += 1
-    if millis - prevMillis > 1000:
-        print(framecount)
-        prevMillis = millis 
-        framecount = 0
-"""
-yetanother = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{[]?-_+~<>i!lI;:,^`'."
-azcueGrayScale = "█▓▒░$%@#≡;:=_-,.'`                                              "
-#azcueGrayScale = "█▓▒░░≡=-,
-test= "▁▂▃▄▅▆▇▉█"
-
-
-scale = "█"
-def nose(y):
-    return int((y*len(scale))//500)
-
-
-
-while True:
-    __, frame = cap.read()
-    #print(len(frame))
-    #print(len(frame[0]))
-    #print(len(frame[0][0]))
-    #print()
-    
-    scale_percent = 10 # percent of original size
-    imgWidth = int(width * scale_percent / 100)
-    imgHeight = int(height * scale_percent / 100)
-    dim = (imgWidth, imgHeight)
-    newFrame = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
-    output = ""
-    for row in newFrame:
-        for pixel in row:
     
 
-            b = int(pixel[0])
-            g = int(pixel[1])
-            r = int(pixel[2])
+    while True:
+        __, frame = cap.read()  
+        scale_percent = scale # percent of original size
+        imgWidth = int(width * scale_percent / 100)
+        imgHeight = int(height * scale_percent / 100)
+        dim = (imgWidth, imgHeight)
+        newFrame = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
+        output = ""
+        for row in newFrame:
+            for pixel in row:
+                b = int(pixel[0])
+                g = int(pixel[1])
+                r = int(pixel[2])
+                y = 0.299 * r +  0.587 * g + 0.114 * b
+                if color:
+                    output += cadd(rgb(r,g,b), asciiChars[int((y*len(asciiChars))//500)]*2)
+                else:
+                    output += asciiChars[int((y*len(asciiChars))//500)]*2
+                
+            output += "\n"
+        print(output)
+        stdout.flush()
 
-            
-            y = 0.299 * r +  0.587 * g + 0.114 * b
-            output += cadd(rgb(r,g,b), scale[nose(y)]*2)
-            #output += scale[nose(y)]*2
-            
-        output += "\n"
-    print(output)
-    stdout.flush()
+        k = cv2.waitKey(1) & 0xff
+        if k == 27:
+            break
 
-    #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #blur = cv2.blur(frame, (5, 5))
-    #ret, thresh = cv2.threshold(blur, 170, 255, 0)
-    #cv2.imshow("Image", frame)
+    cap.release()
+    cv2.destroyAllWindows()
 
 
-    #fpsCount()   
-    k = cv2.waitKey(1) & 0xff
-    if k == 27:
-        break
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Camera feed in terminal with ascii")
+    parser.add_argument("--string", "-S", type=str, nargs="?", default="standard", help="string used (oldTV, standard, standardSmall and pixelated)")
+    parser.add_argument("--customString", "-cs", type=str, nargs="?", default="NA", help="string used")
+    parser.add_argument("--scale", "-s", type=int, nargs="?", default=30, help="scale of the camera feed")
+    parser.add_argument("--color", "-c", type=bool, nargs="?", default=False, help="do you want color?")
+    
+    ns = parser.parse_args(argv[1:])
 
-cap.release()
-cv2.destroyAllWindows()
+    asciiChars={
+        "standard": "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{[]?-_+~<>i!lI;:,^`'.      "[::-1],
+        "standardSmall": " .:-=+*#%@",
+        "oldTV": "▁▂▃▄▅▆▇▉█",
+        "pixelated": "█"
+    }
+
+    if ns.customString == "NA":
+        asciiChar = asciiChars[ns.string]
+    else:
+        asciiChar = ns.customString
+
+    main(asciiChar,ns.scale,ns.color)
